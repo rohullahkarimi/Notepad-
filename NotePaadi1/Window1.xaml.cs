c935138f4fb6e8bb1013296e03e47ed7b1ce79f6
Rohullah Karimi <rohullahahmad99@gmail.com>
2019-11-25 23:05:49 +0200

    add file to repository

---
+++ NotePaadi1/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotePaadi1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            // Accept the dialog and return the dialog result
+            this.DialogResult = true;
+            Close();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
+
